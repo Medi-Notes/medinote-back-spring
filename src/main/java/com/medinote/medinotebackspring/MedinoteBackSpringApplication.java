cab7da777e36b87db7fcd6e1df8eda25d6b45341
J2yun <alex68566856@gmail.com>
2024-04-07 22:36:12 +0900

    project init

---
+++ src/main/java/com/medinote/medinotebackspring/MedinoteBackSpringApplication.java
@@ -0,0 +1,13 @@
+package com.medinote.medinotebackspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedinoteBackSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedinoteBackSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medinote/medinotebackspring/MedinoteBackSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medinote.medinotebackspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedinoteBackSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
