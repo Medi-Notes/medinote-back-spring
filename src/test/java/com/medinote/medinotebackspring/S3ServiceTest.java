bf9e14cdfa3b07271acf81f9896fa9b797036d28
J2yun <alex68566856@gmail.com>
2024-09-10 11:41:03 +0900

    build: 잘못된 테스트 삭제

--- src/test/java/com/medinote/medinotebackspring/S3ServiceTest.java
+++ src/test/java/com/medinote/medinotebackspring/S3ServiceTest.java
@@ -22,34 +22,34 @@ public class S3ServiceTest {
     private final S3Config s3Config = new S3Config();
 
 
-    @Test
-    void getFileTest() {
-        String username = "jiyun";
-        String filename = "940383cf-3f53-4120-8d9b-12ec56c35af4___medinote example audio.mp3";
-
-        S3Client s3 = s3Config.s3Client();
-
-        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
-                .bucket(bucketName)
-                .key(String.format("%s/%s",username,filename))
-                .build();
-
-        ResponseInputStream<GetObjectResponse> s3Response = s3.getObject(getObjectRequest);
-
-
-//        s3.getObjectAsBytes(getObjectRequest);
-
-//        ResponseBytes<GetObjectResponse> s3ObjectBytes = s3.getObjectAsBytes(getObjectRequest);
-
-        try {
-            // Create a file and write the data to it
-            File localFile = new File("testfile.mp3");
-            try (FileOutputStream fos = new FileOutputStream(localFile)) {
-                fos.write(s3Response.readAllBytes());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
+//    @Test
+//    void getFileTest() {
+//        String username = "jiyun";
+//        String filename = "940383cf-3f53-4120-8d9b-12ec56c35af4___medinote example audio.mp3";
+//
+//        S3Client s3 = s3Config.s3Client();
+//
+//        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+//                .bucket(bucketName)
+//                .key(String.format("%s/%s",username,filename))
+//                .build();
+//
+//        ResponseInputStream<GetObjectResponse> s3Response = s3.getObject(getObjectRequest);
+//
+//
+////        s3.getObjectAsBytes(getObjectRequest);
+//
+////        ResponseBytes<GetObjectResponse> s3ObjectBytes = s3.getObjectAsBytes(getObjectRequest);
+//
+//        try {
+//            // Create a file and write the data to it
+//            File localFile = new File("testfile.mp3");
+//            try (FileOutputStream fos = new FileOutputStream(localFile)) {
+//                fos.write(s3Response.readAllBytes());
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
 }
