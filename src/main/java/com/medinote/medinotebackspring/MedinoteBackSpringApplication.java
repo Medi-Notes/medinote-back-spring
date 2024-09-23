9175df3861adb20c7887d79d355c1ecb8f720fc1
J2yun <alex68566856@gmail.com>
2024-09-24 01:25:54 +0900

    Timezone 서울로 설정

--- src/main/java/com/medinote/medinotebackspring/MedinoteBackSpringApplication.java
+++ src/main/java/com/medinote/medinotebackspring/MedinoteBackSpringApplication.java
@@ -2,9 +2,12 @@ package com.medinote.medinotebackspring;
 
 import com.medinote.medinotebackspring.config.properties.AppProperties;
 import com.medinote.medinotebackspring.openai.OpenAIProperties;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
@@ -12,6 +15,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 		OpenAIProperties.class
 })
 public class MedinoteBackSpringApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedinoteBackSpringApplication.class, args);
